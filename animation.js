aea771ccf9b1a876fcfba90b80fa920267afda6e
SamyZarour <zarour.samy@gmail.com>
2015-09-28 23:53:05 -0400

    added bro's files

---
+++ animation.js
@@ -0,0 +1,35 @@
+function function1(){
+	document.getElementById("my1").style.color = "blue";
+	document.getElementById("my2").style.color = "blue";
+	document.getElementById("my3").style.color = "blue";
+};
+function function2(){
+	document.getElementById("my1").style.color = "green";
+	document.getElementById("my2").style.color = "green";
+	document.getElementById("my3").style.color = "green";
+};
+function function3(){
+	document.getElementById("my1").style.color = "yellow";
+	document.getElementById("my2").style.color = "yellow";
+	document.getElementById("my3").style.color = "yellow";
+};
+function function4(){
+	document.getElementById("my1").style.color = "orange";
+	document.getElementById("my2").style.color = "orange";
+	document.getElementById("my3").style.color = "orange";
+};
+function function5(){
+	document.getElementById("my1").style.color = "red";
+	document.getElementById("my2").style.color = "red";
+	document.getElementById("my3").style.color = "red";
+};
+function function6(){
+	document.getElementById("my1").style.color = "violet";
+	document.getElementById("my2").style.color = "violet";
+	document.getElementById("my3").style.color = "violet";
+};
+function function7(){
+	document.getElementById("my1").style.color = "blue";
+	document.getElementById("my2").style.color = "red";
+	document.getElementById("my3").style.color = "green";
+};
